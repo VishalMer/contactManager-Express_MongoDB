dea11bc036d56291eea63481bc2ba48229198593
VishalMer <vishalmer99747@gmail.com>
2025-07-03 23:18:39 +0530

    feat: Enhanced CRUD operstion - contactController

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -43,14 +43,31 @@ const getContact = asyncHandler (async (req, res) => {
 //@route PUT/api/contacts/:id
 //access public
 const updateContact = asyncHandler (async (req, res) => {
-    res.status(200).json({message:`Updated contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact Not Found");
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new : true}
+    );
+    res.status(200).json(updatedContact);
 });
 
 //@desc Delete contact
 //@route DELETE/api/contacts/:id
 //access public
-const deleteContact = asyncHandler (async (req, res) => {
-    res.status(200).json({message:`Deleted contact for ${req.params.id}`});
+ const deleteContact = asyncHandler (async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    await Contact.remove();
+    res.status(200).json(this.deleteContact);
 });
 
 module.exports = {getContacts, CreateContact, getContact, updateContact, deleteContact};
